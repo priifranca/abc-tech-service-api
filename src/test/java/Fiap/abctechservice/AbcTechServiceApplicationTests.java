b731705a39853737a68891b172530aec5199c88e
Priscila <priscila.francas@gmail.com>
2022-04-05 22:08:41 -0300

    First commit

---
+++ src/test/java/Fiap/abctechservice/AbcTechServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package Fiap.abctechservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcTechServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
