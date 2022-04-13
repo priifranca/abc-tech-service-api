f4f0514df23988320b500e3b1a2e982d8d643e6d
Priscila <priscila.francas@gmail.com>
2022-04-12 21:35:16 -0300

    Component

--- src/main/java/Fiap/abctechservice/controller/HealthCheckController.java
+++ src/main/java/Fiap/abctechservice/controller/HealthCheckController.java
@@ -1,34 +1,29 @@
 package Fiap.abctechservice.controller;
 
+import Fiap.abctechservice.application.PropertiesComponent;
+import org.springframework.stereotype.Component;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
 
 @RestController
 @RequestMapping("/")
 
 public class HealthCheckController {
 
+    private PropertiesComponent propertiesComponent;
+    public HealthCheckController(@Autowired PropertiesComponent propertiesComponent){
+        this.propertiesComponent = propertiesComponent;
+    }
     @GetMapping()
     public ResponseEntity<String> status() {
         return ResponseEntity.ok("Sucesso!");
     }
     @GetMapping("version")
     public ResponseEntity<String> version() {
-
-        Properties properties = new Properties();
-        InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.yml");
-        try {
-            properties.load(inputStream);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        return ResponseEntity.ok(properties.getProperty("build.name") + " - " + properties.getProperty("build.version"));
+        return ResponseEntity.ok(this.propertiesComponent.getName() + " - " + this.propertiesComponent.getVersion());
     }
 }
